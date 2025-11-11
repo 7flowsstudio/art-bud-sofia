859ce10788baa66dda9527715903c0805390e72c
VitaliiBarabash1990 <alievruslan1818@gmail.com>
2025-11-11 18:36:22 +0100

    add first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import "./variables.css";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+
+export const metadata: Metadata = {
+	title: "art-bud-sofia",
+	description: "Niezawodne remontu od profesjonalisty w Twoim mieście",
+	icons: {
+		icon: "/favicon.png",
+	},
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="pl">
+			<body>
+				<Header />
+				<main>{children}</main>
+				<Footer />
+			</body>
+		</html>
+	);
+}
