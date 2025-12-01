6cdde97a2af567444bff59603b58ce39cf3f7802
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2025-12-01 16:41:47 +0200

    first-sett-block

---
+++ src/components/Main/Hero/Block/Block.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import s from "./Block.module.css";
+import Image from "next/image";
+
+const Block = () => {
+  return (
+    <div className={s.containerBlock}>
+      <Image
+        src="/img/hero/photo_block.png"
+        alt="work"
+        width={77}
+        height={64}
+        className={s.imgBlock}
+      />
+      <p>Jednym zarządem – gwarancja jakości bez kompromisów.</p>
+      <button className={s.btnBlock}>
+        <span className={s.btnMob}>
+          <Image src="/img/hero/arrow.svg" alt="icon" width={16} height={16} />
+        </span>
+
+        <span className={s.btnDesc}>Uzyskaj konsultację</span>
+      </button>
+    </div>
+  );
+};
+
+export default Block;
--- src/components/Main/Hero/Hero.tsx
+++ src/components/Main/Hero/Hero.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import s from "./Hero.module.css";
+import Block from "./Block/Block";
 
 const Hero = () => {
   return (
     <div className={s.container}>
+      <Block />
       <h1>NIEZAWODNE REMONTU OD PROFESJONALISTY W TWOIM MIEŚCIE</h1>
     </div>
   );
