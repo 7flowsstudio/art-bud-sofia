f26c901e71e1e788a7e50267ccc01a08100c1f29
7flowsstudio <seven.flows.studio@gmail.com>
2025-12-11 21:04:24 +0100

    corect footer description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Benefits from "@/components/Main/Benefits/Benefits";
 import Call from "@/components/Main/Call/Call";
 import Facts from "@/components/Main/Facts/Facts";
 import Form from "@/components/Main/Form/Form";
+import Gallery from "@/components/Main/Gallery/Gallery";
 import Works from "@/components/Main/Works/Works";
 import United from "@/components/United/United";
 
@@ -14,6 +15,7 @@ export default function Home() {
 			<Works />
 			<Benefits />
 			<Call />
+			<Gallery />
 			<Form />
 		</>
 	);
