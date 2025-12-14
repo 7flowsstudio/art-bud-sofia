6bce808494d5c30e45b83b8a6a9165bed927b950
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2025-12-14 20:05:32 +0200

    add-btns-menuSide

--- src/components/Admin/AdminPage/OptionSide/OptionSide.tsx
+++ src/components/Admin/AdminPage/OptionSide/OptionSide.tsx
@@ -1,8 +1,24 @@
+"use client";
 import React from "react";
 import s from "./OptionSide.module.css";
+import { AdminSection } from "../MenuSide/MenuSide";
 
-const OptionSide = () => {
-	return <div className={s.optionSideWrapper}>OptionSide</div>;
+type OptionSideProps = {
+  active: AdminSection;
+};
+
+// const OptionSide = () => {
+//   return <div className={s.optionSideWrapper}>OptionSide</div>;
+// };
+
+const OptionSide = ({ active }: OptionSideProps) => {
+  if (active === "gallery")
+    return <div className={s.optionSideWrapper}>Gallery</div>;
+  if (active === "reviews")
+    return <div className={s.optionSideWrapper}>Reviews</div>;
+  if (active === "services")
+    return <div className={s.optionSideWrapper}>Services</div>;
+  return null;
 };
 
 export default OptionSide;
