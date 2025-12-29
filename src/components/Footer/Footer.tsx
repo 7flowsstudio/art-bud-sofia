6720c367518bb63dc7c7e383b446b15e827104a3
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2025-12-29 15:12:50 +0200

    add-btn-admin

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,65 +9,67 @@ import ListSocial from "./ListSocial/ListSocial";
 import ButtonOnTop from "./ButtonOnTop/ButtonOnTop";
 import { usePathname } from "next/navigation";
 import useScrollAnimation from "@/utils/UseScrollAnimation/useScrollAnimation";
+import Link from "next/link";
+import Router from "next/router";
 const Footer = () => {
-	const [heroPhraseRef, heroPhraseVisible] = useScrollAnimation() as [
-		React.RefObject<HTMLDivElement>,
-		boolean
-	];
-	const [heroNavigateRef, heroNavigateVisible] = useScrollAnimation() as [
-		React.RefObject<HTMLDivElement>,
-		boolean
-	];
-	const [heroCopyrightRef, heroCopyrightVisible] = useScrollAnimation() as [
-		React.RefObject<HTMLDivElement>,
-		boolean
-	];
-	const path = usePathname().split("/")[1];
-	console.log("Path", path);
-	return (
-		<>
-			{path !== "admin" && (
-				<div className={s.sectionFooter}>
-					<div className={`container`}>
-						<div className={s.footer}>
-							<ButtonOnTop />
-							<div className={s.content}>
-								<div
-									ref={heroPhraseRef}
-									className={`${s.logoPhrase} ${s.animatePhrase} ${
-										heroPhraseVisible ? s.visible : ""
-									}`}
-								>
-									<LogoFooter />
-									<Description />
-								</div>
-								<div
-									ref={heroNavigateRef}
-									className={`${s.navigate} ${s.animateNavigate} ${
-										heroNavigateVisible ? s.visible : ""
-									}`}
-								>
-									<ListStacks />
-									<ListInfo />
-									<ListSocial />
-								</div>
-							</div>
-							<div className={s.copyright}>
-								<h4
-									ref={heroCopyrightRef}
-									className={`${s.copyrightTitle} ${s.animateCopyright} ${
-										heroCopyrightVisible ? s.visible : ""
-									}`}
-								>
-									© 2025 Art Bud Sofia. Wszelkie prawa zastrzeżone.
-								</h4>
-							</div>
-						</div>
-					</div>
-				</div>
-			)}
-		</>
-	);
+  const [heroPhraseRef, heroPhraseVisible] = useScrollAnimation() as [
+    React.RefObject<HTMLDivElement>,
+    boolean
+  ];
+  const [heroNavigateRef, heroNavigateVisible] = useScrollAnimation() as [
+    React.RefObject<HTMLDivElement>,
+    boolean
+  ];
+  const [heroCopyrightRef, heroCopyrightVisible] = useScrollAnimation() as [
+    React.RefObject<HTMLDivElement>,
+    boolean
+  ];
+  const path = usePathname().split("/")[1];
+  console.log("Path", path);
+  return (
+    <>
+      {path !== "admin" && (
+        <div className={s.sectionFooter}>
+          <div className={`container`}>
+            <div className={s.footer}>
+              <ButtonOnTop />
+              <div className={s.content}>
+                <div
+                  ref={heroPhraseRef}
+                  className={`${s.logoPhrase} ${s.animatePhrase} ${
+                    heroPhraseVisible ? s.visible : ""
+                  }`}
+                >
+                  <LogoFooter />
+                  <Description />
+                </div>
+                <div
+                  ref={heroNavigateRef}
+                  className={`${s.navigate} ${s.animateNavigate} ${
+                    heroNavigateVisible ? s.visible : ""
+                  }`}
+                >
+                  <ListStacks />
+                  <ListInfo />
+                  <ListSocial />
+                </div>
+              </div>
+              <div className={s.copyright}>
+                <h4
+                  ref={heroCopyrightRef}
+                  className={`${s.copyrightTitle} ${s.animateCopyright} ${
+                    heroCopyrightVisible ? s.visible : ""
+                  }`}
+                >
+                  © 2025 Art Bud Sofia. Wszelkie prawa zastrzeżone.
+                </h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Footer;
